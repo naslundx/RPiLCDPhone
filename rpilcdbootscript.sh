c7ef657258202f0e4e1a91ea26d04bdce8265901
Marcus Näslund <naslundx@gmail.com>
2016-09-17 15:40:12 +0000

    Boot script bug fixes

--- rpilcdbootscript.sh
+++ rpilcdbootscript.sh
@@ -2,7 +2,8 @@ cd ~
 sudo rm -rf RPiLCDPhone/
 git clone https://github.com/naslundx/RPiLCDPhone
 
-cp ~/RPiLCDPhone/rpilcdbootscript.sh ~/rpilcdbootscript.sh
 sh ~/stopserial.sh
 sudo sh ~/stopserial.sh
 sudo python lcd-phone.py &
+
+cp ~/RPiLCDPhone/rpilcdbootscript.sh ~/rpilcdbootscript.sh
