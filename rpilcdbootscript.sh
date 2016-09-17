464420b3dc361632689fffa226d9910b49c00f48
Marcus Näslund <naslundx@gmail.com>
2016-09-17 15:48:37 +0000

    Working boot script

--- rpilcdbootscript.sh
+++ rpilcdbootscript.sh
@@ -1,8 +1,9 @@
 sudo rm -rf /home/pi/RPiLCDPhone/
+cd /home/pi/
 git clone https://github.com/naslundx/RPiLCDPhone
 
 sh /home/pi/RPiLCDPhone/stopserial.sh
 sudo sh /home/pi/RPiLCDPhone/stopserial.sh
-sudo python /home/pi/RPiLCDPhone/lcd-phone.py &
+sudo python /home/pi/RPiLCDPhone/lcd-phone.py
 
 cp /home/pi/RPiLCDPhone/rpilcdbootscript.sh /home/pi/rpilcdbootscript.sh
