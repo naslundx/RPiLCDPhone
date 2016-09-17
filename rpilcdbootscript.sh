79180ff8fa4dcd0310f7bd9b6426418842b3d7d2
Marcus Näslund <naslundx@gmail.com>
2016-09-17 15:43:31 +0000

    asdlk

--- rpilcdbootscript.sh
+++ rpilcdbootscript.sh
@@ -1,9 +1,8 @@
-cd ~
-sudo rm -rf RPiLCDPhone/
+sudo rm -rf /home/pi/RPiLCDPhone/
 git clone https://github.com/naslundx/RPiLCDPhone
 
-sh ~/stopserial.sh
-sudo sh ~/stopserial.sh
-sudo python lcd-phone.py &
+sh /home/pi/RPiLCDPhone/stopserial.sh
+sudo sh /home/pi/RPiLCDPhone/stopserial.sh
+sudo python /home/pi/RPiLCDPhone/lcd-phone.py &
 
-cp ~/RPiLCDPhone/rpilcdbootscript.sh ~/rpilcdbootscript.sh
+cp /home/pi/RPiLCDPhone/rpilcdbootscript.sh /home/pi/rpilcdbootscript.sh
