f200f42ec147436af26694ea1320daa52e2bdf91
Marcus Näslund <naslundx@gmail.com>
2016-09-24 17:13:54 +0200

    Updated boot script

--- rpilcdbootscript.sh
+++ rpilcdbootscript.sh
@@ -4,6 +4,10 @@ git clone https://github.com/naslundx/RPiLCDPhone
 
 sh /home/pi/RPiLCDPhone/stopserial.sh
 sudo sh /home/pi/RPiLCDPhone/stopserial.sh
+cp /home/pi/RPiLCDPhone/rpilcdbootscript.sh /home/pi/rpilcdbootscript.sh
+
+# sudo update-rc.d lightdm disable  # Make sure X server is off
+
 sudo python /home/pi/RPiLCDPhone/lcd-phone.py
 
-cp /home/pi/RPiLCDPhone/rpilcdbootscript.sh /home/pi/rpilcdbootscript.sh
+# sudo reboot  # Reboot on failure
